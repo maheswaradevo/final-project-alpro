8d1a089501f3a5b97a2bbcf3dc6e1e05ba184ab7
Maheswara Devo <pundadevo21@gmail.com>
2021-05-17 09:22:50 +0800

    Masukin fungsi parkir ke fungsi main

--- main.c
+++ main.c
@@ -85,13 +85,39 @@ void game()
     }
     
 }
+
+typedef struct {
+    char type[20];
+    int price;
+}element;
+
+int costCountA(int mobil, int jam)
+{
+    int result;
+    result = jam * mobil;
+    printf("HARGA PARKIR = %d", result);
+    return 0;
+}
+int costCountB(int motor, int jam)
+{
+    int result;
+    result = jam * motor;
+    printf("HARGA PARKIR = %d", result);
+    return 0;
+}
+
 int main()
 {   
     int i, choice;
+    int result;
+    element data[50];
+    FILE *fp;
+    int jam, mnt, dtk, i=0, choice;
     char name[50];
     while(1){
         printf("\n1. MASUKKAN IDENTITAS ANDA\n");
         printf("2. REKOMENDASI PERMAINAN\n");
+        printf("5. HITUNG PARKIR\n");
         printf("6. Exit\n");
         printf("MASUKKAN PILIHAN : ");
         scanf("%d", &choice);
@@ -103,6 +129,35 @@ int main()
             case 2:
                 game();
             break;
+            case 5:
+                fp = fopen("parkir.txt", "r");
+                if(fp == NULL){
+                    printf("Tidak ada file yang ditemukan!");
+                }
+                while(!feof(fp)){
+                    fscanf(fp, "%s %d", &data[i].type, &data[i].price);
+                    i++;
+                }
+                printf("PEMBAYARAN MOBIL PERJAM = %d\n", data[0].price);
+                printf("PEMBAYARAN MOTOR PERJAM = %d\n", data[1].price);
+                printf("KENDARAAN PELANGGAN : \n");
+                printf("1. MOBIL\n");
+                printf("2. MOTOR\n");
+                scanf("%d", &choice);
+                switch(choice){
+                    case 1:
+                    printf("\nMASUKKAN BERAPA LAMA PELANGGAN BERADA DI LOKASI (H/M/S) : ");
+                    scanf("%d %d %d", &jam, &mnt, &dtk);
+                    printf("WAKTU PELANGGAN DI LOKASI : %d JAM %d MENIT %d DETIK", jam, mnt, dtk);
+                    costCountA(data[0].price, jam);
+                    break;
+                    case 2:
+                    printf("\nMASUKKAN BERAPA LAMA PELANGGAN BERADA DI LOKASI (H/M/S) : ");
+                    scanf("%d %d %d", &jam, &mnt, &dtk);
+                    printf("WAKTU PELANGGAN DI LOKASI : %d JAM %d MENIT %d DETIK", jam, mnt, dtk);
+                    costCountB(data[1].price, jam);
+                    break;
+                }
             break;
             case 6:
                 exit(1);
