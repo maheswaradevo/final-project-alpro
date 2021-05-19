cc93e6eacc42c99fb571d6061baab009702efabc
DewaBiara <angabiara@gmail.com>
2021-05-19 08:51:29 +0800

    add void and struct

--- souvenir-shop.c
+++ souvenir-shop.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+void menu();
+void cari();
+void pembelian();
+void daftar_souvenir();
+void tambah_souvenir();
+
+typedef struct{
+    char nama[50];
+    int no;
+    int jmlh;
+    int harga;
+}Souvenir;
+
 
 int main(){
     printf("Hello World");
